0a7aff82d9418e394a498e85e5c089225c440daa
“Dileep <dileepyarra16@gmail.com>
2024-04-30 22:44:02 -0500

    Initial commit

---
+++ src/test/java/com/employee/management/springboot/EmployeeWebApp/EmployeeWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.employee.management.springboot.EmployeeWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
